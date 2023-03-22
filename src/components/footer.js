63da22b730d1bb69746ccd3c27e86b1200f0396e
Jacob Postill <postilljacob@gmail.com>
2023-03-22 19:03:29 -0400

    worked on header, body, and footer

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="https://github.com/your-username">
+        <img src="github-logo.png" alt="Github" />
+      </a>
+      <a href="https://www.linkedin.com/in/your-username/">
+        <img src="linkedin-logo.png" alt="LinkedIn" />
+      </a>
+      <a href="https://www.instagram.com/your-username/">
+        <img src="instagram-logo.png" alt="Instagram" />
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
